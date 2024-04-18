4d27e19a073235f80eb061477bde42fb72efeda3
tymekziomal <166690542+tymekziomal@users.noreply.github.com>
2024-04-18 08:44:21 +0200

    wip: add user printing funcitionality

---
+++ main.py
@@ -0,0 +1,18 @@
+imie="Dawid"
+nazwisko="Baluka"
+liczba_postow=6000
+
+print(f"Twoj znajomy {imie} {nazwisko} opublikowal {liczba_postow} postow")
+
+imie_2="Kewin"
+nazwisko_2="Czajkowski"
+liczba_postow_2=6002
+
+print(f"Twoj znajomy {imie_2} {nazwisko_2} opublikowal {liczba_postow_2} postow")
+
+imie_3="Kamil"
+nazwisko_3="Gil"
+liczba_postow_3=1_000_000
+
+print(f"Twoj znajomy {imie_3} {nazwisko_3} opublikowal {liczba_postow_3} postow")
+
