2b23908c89e02199c65de5352b6b45b43790f000
tymekziomal <166690542+tymekziomal@users.noreply.github.com>
2024-04-18 10:03:29 +0200

    wip: refactoring to folders

---
+++ models/data.py
@@ -0,0 +1,7 @@
+users: list[dict] = [
+    {"name": "Dawid", "surname": "Baluka", "posts": 6000},
+    {"name": "Kewin", "surname": "Czajkowski", "posts": 6002},
+    {"name": "Kamil", "surname": "Gil", "posts": 1_000_000},
+    {"name": "Daniel", "surname": "Blaszczyk", "posts": 6}
+
+]
